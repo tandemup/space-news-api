8dfd5cd63af302146260c6b44fc444d87d349b75
tandemup <ustaticdb@gmail.com>
2023-07-29 14:25:59 +0200

    cate

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div>
       <h1>SpaceX Flights</h1>
+      <p>
+        El que calla por vergüenza o malicia algún pecado grave que ha
+        cometido...o hace su confesión sin dolor o sin propósito o sin ánimo de
+        cumplir la penitencia, no sólo no se confiesa bien sino que comete un
+        grave sacrilegio y queda con la obligación de volver a confesar los
+        pecados que confesó y los que no confesó, más el sacrilegio que hizo.
+      </p>
       <SpaceFlight />
     </div>
   );
